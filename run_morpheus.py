45d555a38bcb2722a18988a51abe75634746493d
mhuertascompany <marc.huertas@obspm.fr>
2022-06-25 17:42:06 +0100

    first commit

---
+++ run_morpheus.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib 
+from morpheus.classifier import Classifier
+from morpheus.data import example
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import pickle
+
+# open image
+hdul = fits.open("/net/diva/scratch-ssd1/mhuertas/data/CEERS/ceers5_f150w_i2d.fits.gz")
+
+image =  hdul[1].data
+
+# convert to e/s
+image_electrons = image/hdul[1].header['PHOTMJSR']
+
+classified = Classifier.classify(h=image_electrons, j=image_electrons, v=image_electrons, z=image_electrons)
+
+#save classifier
+file = open('/net/diva/scratch-ssd1/mhuertas/data/CEERS/classifier_morpheus_ceers5_f150w_i2d.pkl', 'w')
+pickle.dump(classified, file)
+file.close()
+
+mask = np.zeros_like(image_electrons, np.int)
+mask[5:-5, 5:-5] = 1
+
+
+#create and write segmap
+segmap = Classifier.segmap_from_classified(classified, image_electrons, mask=mask)
+hdu = fits.PrimaryHDU(segmap)
+hdul = fits.HDUList([hdu])
+hdul.writeto('/net/diva/scratch-ssd1/mhuertas/data/CEERS/segmap_morpheus_ceers5_f150w_i2d.fits')
+
+catalog = Classifier.catalog_from_classified(classified, image_electrons, segmap)
+
+#write catalog
+file = open('/net/diva/scratch-ssd1/mhuertas/data/CEERS/catalog_morpheus_ceers5_f150w_i2d.pkl', 'w')
+pickle.dump(catalog, file)
+file.close()
+
+
+
+
+
