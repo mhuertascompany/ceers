81352df02e36fcff0cdb372a2b3b9e25f2207227
mhuertascompany <marc.huertas@obspm.fr>
2022-09-12 16:08:08 +0200

    GPU1

--- adversarial/adversarial_training.py
+++ adversarial/adversarial_training.py
@@ -23,7 +23,7 @@ import os
 import matplotlib.pyplot as plt
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 
 ## read data
