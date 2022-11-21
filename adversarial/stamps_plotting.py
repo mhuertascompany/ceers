8349297334518898dd2c0fb1be7d0fe0103d978b
mhuertascompany <marc.huertas@obspm.fr>
2022-11-21 12:31:23 -0500

    index

--- adversarial/stamps_plotting.py
+++ adversarial/stamps_plotting.py
@@ -108,8 +108,8 @@ with PdfPages(data_path+'figures/sph_CEERS_f200w.pdf') as pdf_ceers,PdfPages(dat
             
 
                     if read ==1:
-                        nir_f160=wfc3_f160_list[0]
-                        stamp_candels =Cutout2D(nir_f160[0].data,position,32,wcs = wf160[0])
+                        nir_f160=wfc3_f160_list[k-1]
+                        stamp_candels =Cutout2D(nir_f160[0].data,position,32,wcs = wf160[k-1])
                         hdu = fits.PrimaryHDU(stamp_candels.data)
                         hdu.writeto('tmp_candels.fits', overwrite=True) 
                         
