d232630be70a73e93afe33c129da457452f2fceb
mhuertascompany <marc.huertas@obspm.fr>
2022-11-22 12:07:54 -0500

    cosmetics

--- adversarial/stamps_plottting_filters.py
+++ adversarial/stamps_plottting_filters.py
@@ -111,10 +111,13 @@ def plot_stamps(wl,morph,ceers_cat,nir_f200_list,w):
                             gc.add_scalebar(0.1 * u.arcsec)
                             #gc.scalebar.set_length(0.1/0.03 * u.pixel)
                             gc.scalebar.set_label(str(kpc_per_arcsec*0.1))
-                            gc.scalebar.set_corner('top right')
+                            gc.scalebar.set_corner('bottom right')
                             gc.scalebar.set_color('black')
                             gc.scalebar.set_linestyle('solid')
                             gc.scalebar.set_linewidth(3)
+                            gc.scalebar.set_font(size='medium', weight='medium', \
+                      stretch='normal', family='sans-serif', \
+                      style='normal', variant='normal')
                             gc.show_grayscale(stretch='sqrt',invert=True)
 
                             ax_ceers.set_yticklabels([])
