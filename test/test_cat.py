5c5a38314466c3ceda62223de2b93f7fab67dae5
metasophie <2296131461@qq.com>
2023-09-07 18:32:29 +0100

    update

--- test/test_cat.py
+++ test/test_cat.py
@@ -10,7 +10,11 @@ cat_dir = "/scratch/mhuertas/CEERS/data_release/cats"
 cat_name = "CEERS_DR05_adversarial_asinh_4filters_1122_4class_ensemble_v02_stellar_params_morphflag_delta_10points_DenseBasis_galfit_CLASS_STAR_v051_bug.csv"
 
 
-cat = pd.read_csv(os.path.join(cat_dir,cat_name),nrows=10)
+cat = pd.read_csv(os.path.join(cat_dir,cat_name),nrows=10100)
 col_names = ['RA_1','DEC_1']
+cols = cat.columns
 
-print(type(cat[col_names]))
+for col in cols:
+    print(col)
+
+print(cat[['zfit_50','logMt_50','logM_50','F200W_RE','F200W_MAG','star_flag']].values[[10046,10049,10050,10053,10054]])
--- test/test_image.py
+++ test/test_image.py
@@ -2,13 +2,14 @@
 from astropy.io import fits
 from astropy.visualization import simple_norm
 from astropy import wcs
+from astropy.nddata import Cutout2D
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 
 img_dir = "/scratch/ydong/images"
-img_name = "ceers_nircam10_f115w_v0.51_i2d.fits"
+img_name = "ceers_nircam10_f150w_v0.51_i2d.fits"
 
 cat_dir = "/scratch/mhuertas/CEERS/data_release/cats"
 cat_name = "CEERS_DR05_adversarial_asinh_4filters_1122_4class_ensemble_v02_stellar_params_morphflag_delta_10points_DenseBasis_galfit_CLASS_STAR_v051_bug.csv"
@@ -19,32 +20,49 @@ col_names = ['RA_1','DEC_1']
 coords = cat[col_names].values
 
 with fits.open(os.path.join(img_dir,img_name)) as hdul:
-    # hdul.info()
+    print(repr(hdul[0].header))
     hdr = hdul[1].header
     w = wcs.WCS(hdr)
+    print(repr(hdr))
     data = hdul[1].data
 
-zero = np.sum(data==0.)
-plt.imshow(data==0.,cmap='gray')
-plt.savefig('zero.png')
+# zero = np.sum(data==0.)
+# plt.imshow(data==0.,cmap='gray')
+# plt.savefig('zero7.png')
 
-norm = simple_norm(data,'log',percent=99)
-plt.imshow(data,norm=norm,cmap='gray')
-plt.savefig('test.png')
+# fig = plt.figure(figsize=(10, 10))
+# ax = plt.subplot(projection=w)
+# norm = simple_norm(data,'log',percent=98)
+# plt.imshow(data, origin='lower', cmap='cividis', aspect='equal')
+# plt.xlabel(r'Ra')
+# plt.ylabel(r'Dec')
+
+# overlay = ax.get_coords_overlay('icrs')
+# overlay.grid(color='white', ls='dotted')
+
+# plt.savefig('test8.png')
 
 size = 100
 # print(w)
-pixels = w.wcs_world2pix(coords,0).astype(int)
-for i in range(10000):
+print(data.shape)
+ymax, xmax = data.shape
+pixels = w.wcs_world2pix(coords,0)
+for i in range(24320):
     pix = pixels[i]
-    up = pix[0]+size
+    up = int(pix[0]+size)
     down = up-size*2
-    right = pix[1]+size
+    right = int(pix[1]+size)
     left = right-size*2
-    if all([up<4800,down>-1,right<10500,left>-1]):
-        print(i)
-        cut = data[down:up,left:right]
-        norm = simple_norm(cut,'log',percent=99)
-        plt.imshow(cut,norm=norm)
-        plt.savefig('test_%i.png'%i)
+    if all([up<xmax,down>-1,right<ymax,left>-1]):
+        
+        # cut = data[left:right,down:up]
+        cut = Cutout2D(data,pix,wcs=w,size=size*2)
+        if np.max(cut.data)>0:
+            print(i)
+            print(pix,coords[i],w.wcs_pix2world(pix.reshape(-1,2),0))
+            norm = simple_norm(cut.data,'log',percent=99)
+            
+            plt.imshow(cut.data,cmap='gray')
+            
+            plt.savefig('sw10_%i.png'%i)
 
