7783bf52fe3a4e3821cab45ad94a7446137667e2
mhuertascompany <marc.huertas@obspm.fr>
2023-09-14 12:44:41 +0100

    tng

--- progeintors/get_sh.py
+++ progeintors/get_sh.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import illustris_python as il
 
-output_dir="/u/mhuertas/data/CEERS/"
-
+output_dir = "/u/mhuertas/data/CEERS/"
 basePath = '/virgotng/universe/IllustrisTNG/TNG100-1/output/'
 fields = ['SubhaloMass', 'SubhaloSFRinRad']
 subhalos = il.groupcat.loadSubhalos(basePath, 99, fields=fields)
 
-# Convert the subhalos dictionary to a DataFrame
+# Add a new key 'RowNumber' to the subhalos dictionary
+subhalos['SHID'] = range(len(subhalos['SubhaloMass']))
+
+# Convert the modified subhalos dictionary to a DataFrame
 subhalos_df = pd.DataFrame(subhalos)
 
 # Save the DataFrame to a CSV file
-subhalos_df.to_csv(output_dir+'subhalos_data.csv', index=False)
+subhalos_df.to_csv(output_dir + 'subhalos_data.csv', index=False)
