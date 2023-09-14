f27904b26933cd0db06f8ad4dd6b418ba8421547
mhuertascompany <marc.huertas@obspm.fr>
2023-09-14 12:32:08 +0100

    tree

---
+++ progeintors/get_progenitors.py
@@ -0,0 +1,22 @@
+import illustris_python as il
+import pandas as pd
+from astropy.io import fits
+import numpy as np
+
+output_dir="/u/mhuertas/data/CEERS/"
+basePath = '/virgo/simulations/IllustrisTNG/TNG50-1/output/'
+
+
+
+cat = pd.read_csv(output_dir+"subhalos_data.csv")
+fields = ['SubhaloMass','SubfindID','SnapNum']
+
+
+for idn in cat.GalID:
+    tree = il.sublink.loadTree(basePath,99,idn,fields=fields,onlyMPB=True)
+    try:
+        df = pd.DataFrame(list(zip(tree['SnapNum'], tree['SubfindID'])), 
+               columns =['SnapNUm', 'SubfindID','SubhaloMass']) 
+        df.to_csv(output_dir+"TNG100projenitors/TNG100_tree_"+str(idn)+".csv")
+    except:
+        print("Error")  
