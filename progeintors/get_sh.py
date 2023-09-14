7f2c8b43aec7c1400a51dedbf6c53b2e96f4f92b
mhuertascompany <marc.huertas@obspm.fr>
2023-09-14 12:50:24 +0100

    tng

--- progeintors/get_sh.py
+++ progeintors/get_sh.py
@@ -11,6 +11,9 @@ subhalos['SHID'] = range(len(subhalos['SubhaloMass']))
 
 # Convert the modified subhalos dictionary to a DataFrame
 subhalos_df = pd.DataFrame(subhalos)
+mass_msun = subhalos['SubhaloMass'] * 1e10 / 0.704
+
+subhalos_massive = subhalos_df.query('SubhaloMass>1e9*0.704/1e10')
 
 # Save the DataFrame to a CSV file
-subhalos_df.to_csv(output_dir + 'subhalos_data.csv', index=False)
+subhalos_massive.to_csv(output_dir + 'subhalos_data_1e9.csv', index=False)
