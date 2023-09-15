dbf8ffec56b9636ee3978fcdd0aff9d3250f39ff
mhuertascompany <marc.huertas@obspm.fr>
2023-09-15 10:09:10 +0100

    tng

--- progeintors/get_progenitors.py
+++ progeintors/get_progenitors.py
@@ -7,16 +7,17 @@ import pdb
 output_dir="/u/mhuertas/data/CEERS/"
 basePath = '/virgotng/universe/IllustrisTNG/TNG100-1/output/'
 
-GroupFirstSub = il.groupcat.loadHalos(basePath,99,fields=['GroupFirstSub'])
+Halos99 = il.groupcat.loadHalos(basePath,99,fields=['GroupFirstSub','GroupMass','GroupMassType',])
 fields = ['SubhaloMass','SubfindID','SnapNum','SubhaloMassType']
 
 
-for idn in GroupFirstSub:
-    tree = il.sublink.loadTree(basePath,99,idn,fields=fields,onlyMPB=True)
-    #pdb.set_trace()
-    try:
-        df = pd.DataFrame(list(zip(tree['SnapNum'], tree['SubfindID'], tree['SubhaloMass'],tree['SubhaloMassType'][:,4])), 
-               columns =['SnapNUm', 'SubfindID','SubhaloMass','SubhaloMstar']) 
-        df.to_csv(output_dir+"TNG100projenitors/TNG100_tree_"+str(idn)+".csv")
-    except:
-        print("Error")  
+for idn,mass in zip(Halos99['GroupFirstSub'],Halos99['GroupMassType'][:,4]):
+    if np.log10(mass*1e10/0.704)>9
+        tree = il.sublink.loadTree(basePath,99,idn,fields=fields,onlyMPB=True)
+        #pdb.set_trace()
+        try:
+            df = pd.DataFrame(list(zip(tree['SnapNum'], tree['SubfindID'], tree['SubhaloMass'],tree['SubhaloMassType'][:,4])), 
+                columns =['SnapNUm', 'SubfindID','SubhaloMass','SubhaloMstar']) 
+            df.to_csv(output_dir+"TNG100projenitors/TNG100_tree_"+str(idn)+".csv")
+        except:
+            print("Error")  
