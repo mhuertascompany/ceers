31c5cbb400363d92786927fd11ba6256ab1ebe52
mhuertascompany <marc.huertas@obspm.fr>
2023-09-21 11:25:03 +0100

    tng50

--- progeintors/get_progenitors.py
+++ progeintors/get_progenitors.py
@@ -5,7 +5,7 @@ import numpy as np
 import pdb
 
 output_dir="/u/mhuertas/data/CEERS/"
-basePath = '/virgotng/universe/IllustrisTNG/TNG100-1/output/'
+basePath = '/virgotng/universe/IllustrisTNG/TNG50-1/output/'
 
 Halos99 = il.groupcat.loadHalos(basePath,99,fields=['GroupFirstSub','GroupMass','GroupMassType',])
 fields = ['SubhaloMass','SubfindID','SnapNum','SubhaloMassType','SubhaloHalfmassRadType']
@@ -18,6 +18,6 @@ for idn,mass in zip(Halos99['GroupFirstSub'],Halos99['GroupMassType'][:,4]):
         try:
             df = pd.DataFrame(list(zip(tree['SnapNum'], tree['SubfindID'], np.log10(tree['SubhaloMass']*1e10/0.704),np.log10(tree['SubhaloMassType'][:,4]*1e10/0.704),tree['SubhaloHalfmassRadType'][:,4]/0.704)), 
                 columns =['SnapNUm', 'SubfindID','SubhaloMass','SubhaloMstar','SubhaloHalfmassRad']) 
-            df.to_csv(output_dir+"TNG100projenitors_sizemass/TNG100_tree_"+str(idn)+".csv")
+            df.to_csv(output_dir+"TNG100projenitors_sizemass/TNG50_tree_"+str(idn)+".csv")
         except:
             print("Error")  
