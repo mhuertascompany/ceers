60bc405435a6e343b64936e4593d0fc1372f9193
mhuertascompany <marc.huertas@obspm.fr>
2023-09-21 16:05:30 +0100

    QSOs

--- QSOs/get_stamps_CANDELS.py
+++ QSOs/get_stamps_CANDELS.py
@@ -41,84 +41,47 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 
-
-
-def read_CANDELS_data(data_path):
+def read_CANDELS_data_and_save(data_path, output_path):
     
-    candels_cat = pd.read_csv(data_path+"QSOs/COSMOS_QSO_presample.csv")
+    candels_cat = pd.read_csv(data_path + "QSOs/COSMOS_QSO_presample.csv")
     
+    wfc3_f160_list = []
+    wf160 = []
+    candels_images = [
+        "hlsp_candels_hst_wfc3_egs-tot-60mas_f160w_v1.0_drz.fits",
+        "hlsp_candels_hst_wfc3_gs-tot_f160w_v1.0_drz.fits",
+        "hlsp_candels_hst_wfc3_cos-tot_f160w_v1.0_drz.fits",
+        "hlsp_candels_hst_wfc3_uds-tot_f160w_v1.0_drz.fits"
+    ]
+    
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
+    fields = ["egs", "GDS", "COSMOS", "UDS"]
 
-    wfc3_f160_list=[]
-    wf160=[]
-    candels_images = ["hlsp_candels_hst_wfc3_egs-tot-60mas_f160w_v1.0_drz.fits","hlsp_candels_hst_wfc3_gs-tot_f160w_v1.0_drz.fits","hlsp_candels_hst_wfc3_cos-tot_f160w_v1.0_drz.fits","hlsp_candels_hst_wfc3_uds-tot_f160w_v1.0_drz.fits"]
-    for c in candels_images:
-        wfc3_f160 = fits.open(data_path+"images/"+c)
-        wfc3_f160_list.append(wfc3_f160)
-        wf160.append(WCS(wfc3_f160[0].header))
-        wf160[-1].sip = None
-
-
-
-    fields = ["egs","GDS","COSMOS","UDS"]
-    X=[]
-    label=[]
-
-    for wfc3_f160,w_c,f in zip(wfc3_f160_list,wf160,fields): 
-        sel = candels_cat.query('HMAG<24 and FIELD=='+'"'+f+'"')
-        print(len(sel))
-        
-        for idn,ra,dec,fsph,fdk,firr in zip(sel.RB_ID,sel.RA,sel.DEC,sel.F_SPHEROID,sel.F_DISK,sel.F_IRR):
+    for wfc3_f160, w_c, f in zip(wfc3_f160_list, wf160, fields): 
+        for ra, dec in zip(candels_cat.RA, candels_cat.DEC):
+            try:
+                position = SkyCoord(ra, dec, unit="deg")
+                stamp = Cutout2D(wfc3_f160[0].data, position, 32, wcs=w_c)
                 
-                try:
-                    
-                    position = SkyCoord(ra,dec,unit="deg")
-                    #print(ra,dec)
-                    
-                    stamp = Cutout2D(wfc3_f160[0].data,position,32,wcs=w_c)
-                    
-                    
-                    
-                    if np.max(stamp.data)<=0 or np.count_nonzero(stamp.data==0)>10:
-                        continue
-                    
-                    
-                    transform = AsinhStretch() + interval
-                    #transform = LogStretch() + interval
-                    norm = transform(stamp.data)
-                    
-                    #stamp_name = data_path+"NirCam/CANDELS_stamps/v005/f200fullres/CANDELS-CEERS"+str(idn)+"_f200w_v005.fits"
-
-                    if (fsph>=0.66 and fdk<0.66 and firr<0.1):
-                        label.append(0)
-                        
-                        X.append(norm)
-                    elif ((fsph<0.66 and fdk>=0.66 and firr<0.1)):
-                        label.append(1)
-                        X.append(norm)
-                    elif ((firr>=0.1)):
-                        label.append(2) 
-                        X.append(norm)
-                    elif ((fsph>0.66 and fdk>0.66 and firr<0.1)):
-                        label.append(3)
-                        X.append(norm)     
-
-
-                except:
+                if np.max(stamp.data) <= 0 or np.count_nonzero(stamp.data == 0) > 10:
                     continue
 
+                #transform = AsinhStretch() + MinMaxInterval()
+                #norm = transform(stamp.data)
+                
+                # Create a new FITS file for each stamp
+                fits_filename = os.path.join(output_path, f"CANDELS_stamp_ra{ra}_dec{dec}.fits")
+                hdu = fits.PrimaryHDU(stamp.data)
+                hdul = fits.HDUList([hdu])
+                hdul.writeto(fits_filename, overwrite=True)
 
-    return X,label, candels_cat     
+            except:
+                continue
 
-
+# Example usage:
 data_path = "/scratch/mhuertas/CEERS/data_release/"
-X, label, candels_cat = read_CANDELS_data(data_path)
-
-# Add 'label' as a new column to the 'candels_cat' DataFrame
-candels_cat['label'] = label
-
-# Save the 'X' data as an .npz file
-np.savez(data_path+"QSOs/QSOs_CANDELS.npz", X=np.array(X))
-
-# Save the updated 'candels_cat' DataFrame as a CSV file
-candels_cat.to_csv(data_path+"QSOs/COSMOS_QSO_presample_morph.csv", index=False)
+output_path = data_path = "/scratch/mhuertas/CEERS/data_release/QSOs"
+read_CANDELS_data_and_save(data_path, output_path)
