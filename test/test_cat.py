19d89419e008715e9002a5fa5780fbdebcf39761
metasophie <2296131461@qq.com>
2023-10-18 09:25:26 +0100

    update

--- test/test_cat.py
+++ test/test_cat.py
@@ -11,11 +11,19 @@ cat_name = "CEERS_DR05_adversarial_asinh_4filters_1122_4class_ensemble_v02_stell
 
 
 cat = pd.read_csv(os.path.join(cat_dir,cat_name),nrows=10100)
+
+class_dir = "/scratch/ydong/classifications"
+class_name = "jwst-ceers-v0-5-aggregated-class-singlechoicequestionsonly.csv"
+
+cla = pd.read_csv(os.path.join(class_dir,class_name))
+
 col_names = ['RA_1','DEC_1']
-cols = cat.columns
+cols = cla.columns
 
 for col in cols:
     print(col)
+
+print(cla['mag_select'])
 
 for i in range(100):
     print(cat[['ID','RA_1','DEC_1','RA_2','DEC_2']].values[i])
