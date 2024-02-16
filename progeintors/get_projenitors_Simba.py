225d3ab7e6720a7feffaa366f5d70db1ef56aa54
mhuertascompany <marc.huertas@obspm.fr>
2024-02-16 16:02:27 +0000

    simba

---
+++ progeintors/get_projenitors_Simba.py
@@ -0,0 +1,24 @@
+import illustris_python as il
+import pandas as pd
+from astropy.io import fits
+import numpy as np
+import pdb
+
+output_dir="/u/mhuertas/data/CEERS/"
+basePath = '/virgotng/universe/Simba/L100n1024FP/output'
+
+Halos99 = il.groupcat.loadHalos(basePath,28,fields=['GroupFirstSub','GroupMass','GroupMassType',])
+fields = ['SubhaloMass','SubfindID','SnapNum','SubhaloMassType','SubhaloHalfmassRadType','SubhaloSFRinRad','SubhaloMassInRadType']
+
+
+for idn,mass in zip(Halos99['GroupFirstSub'],Halos99['GroupMassType'][:,4]):
+    if np.log10(mass*1e10/0.704)>9:
+        tree = il.sublink.loadTree(basePath,28,idn,fields=fields,onlyMPB=True)
+        #pdb.set_trace()
+        try:
+            
+            df = pd.DataFrame(list(zip(tree['SnapNum'], tree['SubfindID'], np.log10(tree['SubhaloMass']*1e10/0.704),np.log10(tree['SubhaloMassType'][:,4]*1e10/0.704),tree['SubhaloHalfmassRadType'][:,4]/0.704,np.log10(tree['SubhaloMassInRadType'][:,4]*1e10/0.704), tree['SubhaloSFRinRad'])), 
+                columns =['SnapNUm', 'SubfindID','SubhaloMass','SubhaloMstar','SubhaloHalfmassRad','SubhaloMstar2','SubhaloSFR2']) 
+            df.to_csv(output_dir+"Simbaprojenitors_sizemass_sSFR/Simba_tree_"+str(idn)+".csv")
+        except:
+            print("Error")  
