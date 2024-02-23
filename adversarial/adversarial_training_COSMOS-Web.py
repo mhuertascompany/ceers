8ebf8cc5fa06716228d2e102bdc258b86705e176
mhuertascompany <marc.huertas@obspm.fr>
2024-02-23 12:53:45 +0000

    candide

--- adversarial/adversarial_training_COSMOS-Web.py
+++ adversarial/adversarial_training_COSMOS-Web.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
 from tensorflow.python.keras import Model
-#import imageio
+import imageio
 import pandas as pd
 import numpy as np
 import IPython.display as display
