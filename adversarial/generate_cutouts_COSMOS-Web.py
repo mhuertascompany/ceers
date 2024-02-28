ab86340d7cc800a3d10c7b12e90becad9cdff656
mhuertascompany <marc.huertas@obspm.fr>
2024-02-28 12:22:35 +0000

    cutouts

---
+++ adversarial/generate_cutouts_COSMOS-Web.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+
+data_path = "/Users/marchuertascompany/Documents/data/COSMOS-Web"
+# Load your DataFrame
+ceers_cat = pd.read_csv(data_path+'COSMOSWeb_master_v1.6.0-sersic+BD-em_cgs_LePhare_nodupl_nomulti_adversarialF150W.csv')  # Replace this with your actual file path
+
+
+filters = ['F150W']
+morph=['sph','disk','irr','bd']
+
+for f in filters:
+    for m in morph:
+        c = ceers_cat.filter(regex='^'+m+'_')
+        c = c.filter(regex=f+'$')
+        ceers_cat[m+'_'+f+'_mean']=c.mean(axis=1).values
+        ceers_cat[m+'_'+f+'_std']=c.std(axis=1).values
+
+
+
+
+
+
+
+morph_flag=[]
+delta_value = []
+
+for sph,dk,irr,bd in zip(ceers_cat.sph_F150W_mean,ceers_cat.disk_F150W_mean,ceers_cat.irr_F150W_mean,ceers_cat.bd_F150W_mean):
+    maxpos = np.argmax([sph,dk,irr,bd])
+    delta = np.sort([sph,dk,irr,bd])[3]-np.sort([sph,dk,irr,bd])[2]
+    morph_flag.append(maxpos)
+    delta_value.append(delta)
+#morph_flag=np.array(morph_flag)
+#morph_flag[(ceers_cat.disk_f200>0.3)]=1    
+ceers_cat['morph_flag_f150w']=np.array(morph_flag)
+ceers_cat['delta_f150']=np.array(delta_value)
+
+
+#ceers_cat.to_csv(data_path+"CEERS_DR05_adversarial_asinh_4filters_1122_4class_ensemble_v02_stellar_params_morphflag_delta_10points_DenseBasis_galfit_CLASS_STAR_v052_bug.csv")
+
+
+# Define your queries as a list of tuples (query, number_of_ids)
+# Each tuple consists of a query string and the number of ids you want to select for that query
+
+
+
+queries = [
+    ("morph_flag_f150w==0 and MAG_MODEL_F150W<24.5", 20,'sph'),  # Replace with your actual query and number of ids
+    ("morph_flag_f150w==1 and MAG_MODEL_F150W<24.5", 20,'disk'),  # Replace with your actual query and number of ids
+    ("morph_flag_f150w==2 and MAG_MODEL_F150W<24.5", 20,'irr'),
+    ("morph_flag_f150w==3 and MAG_MODEL_F150W<24.5", 20,'bd')
+    # Add more queries as needed
+]
+
+# Base command parts
+base_command = "python /home/shuntov/COSMOS-Web/TheSurvey/makeCutouts.py"
+output_path = "/n03data/huertas/COSMOS-Web/cutouts/"
+cutout_size = 2.0
+
+# Open the shell script file
+with open("/n03data/huertas/COSMOS-Web/cutouts/generate_cutouts.sh", "w") as file:
+    file.write("#!/bin/bash\n")
+
+    # Process each query
+    for query, n,f in queries:
+        # Filter the DataFrame according to the query, select random ids, then sort them in descending order
+        filtered_ids = ceers_cat.query(query)['id_1'].sample(n=n, random_state=1).sort_values(ascending=False)  # Change random_state for different subsets
+        
+        # Create the command with selected ids
+        source_ids_str = ' '.join(map(str, filtered_ids))
+        command = f"{base_command} --source_ids {source_ids_str} --output_path {output_path+f} --cutout_size {cutout_size}\n"
+        
+        # Write the command to the file
+        file.write(command)
+
+print("Shell script 'generate_cutouts.sh' has been created with multiple commands.")
