cf36a97186e9e9d668e4c9ed44cf80284f88d29b
mhuertascompany <marc.huertas@obspm.fr>
2024-04-16 16:00:10 +0100

    cosmos

--- adversarial/adversarial_training_COSMOS-Web.py
+++ adversarial/adversarial_training_COSMOS-Web.py
@@ -47,8 +47,8 @@ import os
 
 import matplotlib.pyplot as plt
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+#os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 
 filters = [
