775a7b28483f6e1d01dc015397e6133a677247a9
mhuertascompany <marc.huertas@obspm.fr>
2024-04-25 12:54:15 +0100

    bars

--- bar_estimate/COSMOS_select_bars.py
+++ bar_estimate/COSMOS_select_bars.py
@@ -71,7 +71,7 @@ draw_max = ImageDraw.Draw(max_bar_image_F200W)
 font = ImageFont.truetype(fm.findfont(fm.FontProperties(family='serif')),size=20)
 
 
-max_bar_ids = np.where(p_bar>0.5)[0]
+max_bar_ids = np.where(p_bar>0.5 and p_feature>0.5 and p_edgeon<0.5)[0]
 i = 0
 for bar_id in max_bar_ids:
     image_path = os.path.join(image_dir, "F150W_%i.jpg"%id[bar_id])
