bc24e741f0e96d3cae9a93c35a5199aa046b3413
mhuertascompany <marc.huertas@obspm.fr>
2024-09-20 11:02:05 +0100

    debug adversarial

--- adversarial/adversarial_training_COSMOS-Web.py
+++ adversarial/adversarial_training_COSMOS-Web.py
@@ -764,7 +764,8 @@ alpha = 1
 nruns = 10  #set to 0 for skip training
 
 #filters=['f150w','f200w','f356w','f444w']
-filters = ['F150W', 'F277W', 'F444W']
+#filters = ['F150W', 'F277W', 'F444W']
+filters = ['F444W']
 train = [1,1,1]
 
 data_path = "/n03data/huertas/CANDELS/"
