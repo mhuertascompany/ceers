3f7d9f11b615a181623333e8453135e6f4087cc0
mhuertascompany <marc.huertas@obspm.fr>
2024-09-27 11:36:48 +0200

    fit DPL

--- adversarial/SMFs/MCMC_SMF_3models.py
+++ adversarial/SMFs/MCMC_SMF_3models.py
@@ -14,10 +14,10 @@ def double_schechter_function(logM, logM_star, alpha1, alpha2, logphi1, logphi2)
     M_over_M_star = logM - logM_star
     return np.log(10) * np.exp(-10**M_over_M_star) * 10**M_over_M_star * (10**logphi1 * 10**(M_over_M_star * alpha1) + 10**logphi2 * 10**(M_over_M_star * alpha2))
 
-def DPL(logM, logM_star, alpha1, alpha2, phi_star):
+def DPL(logM, logM_star, alpha1, alpha2, logphi_star):
     term1 = 10 ** (-(logM - logM_star) * (alpha1 + 1))
     term2 = 10 ** (-(logM - logM_star) * (alpha2 + 1))
-    phi_logM_star = phi_star / (term1 + term2)
+    phi_logM_star = 10**logphi_star / (term1 + term2)
     return phi_logM_star
 
 def log_likelihood_single(theta, logM, Phi, dPhi):
@@ -37,19 +37,19 @@ def log_likelihood_DPL(theta, logM, Phi, dPhi):
 
 def log_prior_single(theta):
     logphi_star, logM_star, alpha = theta
-    if -5 < logphi_star < 0 and 8.0 < logM_star < 12.0 and -4 < alpha < 4:
+    if -7 < logphi_star < 0 and 8.0 < logM_star < 12.0 and -4 < alpha < 4:
         return 0.0
     return -np.inf
 
 def log_prior_double(theta):
     logM_star, alpha1, alpha2, logphi1, logphi2 = theta
-    if -5 < logphi1 < 0 and -5 < logphi2 < 0 and 8.0 < logM_star < 12.0 and -4 < alpha1 < -1 and -1 < alpha2 < 4:
+    if -7 < logphi1 < 0 and -5 < logphi2 < 0 and 8.0 < logM_star < 12.0 and -4 < alpha1 < -1 and -1 < alpha2 < 4:
         return 0.0
     return -np.inf
 
 def log_prior_DPL(theta):
     logM_star, alpha1, alpha2, logphi_star = theta
-    if -5 < logphi_star < 0 and 8.0 < logM_star < 12.0 and -4 < alpha1 < 4 and -4 < alpha2 < 4:
+    if -7 < logphi_star < 0 and 8.0 < logM_star < 12.0 and -4 < alpha1 < 4 and -4 < alpha2 < 4:
         return 0.0
     return -np.inf
 
@@ -113,32 +113,32 @@ def fit_MCMC(smf_morph, path_out, filename, fit_range=(9, 12)):
                     initial_guess_double = [10.5, -0.6, -1.7, -2, -2]
                     pos_double = pos_func_double(initial_guess_double)
                     sampler_double = emcee.EnsembleSampler(nwalkers_double, ndim_double, log_probability_double, args=(logM, Phi, dPhi))
-                    sampler_double.run_mcmc(pos_double, 60000, progress=True)
-                    samples_double = sampler_double.get_chain(discard=10000, thin=15, flat=True)
-                    fit_results[(zbin, morph, 'double')] = {
-                        'sampler': sampler_double,
-                        'params_50': np.percentile(samples_double, 50, axis=0),
-                        'params_16': np.percentile(samples_double, 16, axis=0),
-                        'params_84': np.percentile(samples_double, 84, axis=0)
-                    }
-                    print(f'Done double Schechter fit for {morph} at z={zbin}')
+                    #sampler_double.run_mcmc(pos_double, 60000, progress=True)
+                    #samples_double = sampler_double.get_chain(discard=10000, thin=15, flat=True)
+                    #fit_results[(zbin, morph, 'double')] = {
+                    #    'sampler': sampler_double,
+                    #    'params_50': np.percentile(samples_double, 50, axis=0),
+                    #    'params_16': np.percentile(samples_double, 16, axis=0),
+                    #    'params_84': np.percentile(samples_double, 84, axis=0)
+                    #}
+                    #print(f'Done double Schechter fit for {morph} at z={zbin}')
 
                     # Single Schechter Fit
                     initial_guess_single = [-2, 10.5, -1.2]
                     pos_single = pos_func_single(initial_guess_single)
                     sampler_single = emcee.EnsembleSampler(nwalkers_single, ndim_single, log_probability_single, args=(logM, Phi, dPhi))
-                    sampler_single.run_mcmc(pos_single, 60000, progress=True)
-                    samples_single = sampler_single.get_chain(discard=10000, thin=15, flat=True)
-                    fit_results[(zbin, morph, 'single')] = {
-                        'sampler': sampler_single,
-                        'params_50': np.percentile(samples_single, 50, axis=0),
-                        'params_16': np.percentile(samples_single, 16, axis=0),
-                        'params_84': np.percentile(samples_single, 84, axis=0)
-                    }
-                    print(f'Done single Schechter fit for {morph} at z={zbin}')
+                    #sampler_single.run_mcmc(pos_single, 60000, progress=True)
+                    #samples_single = sampler_single.get_chain(discard=10000, thin=15, flat=True)
+                    #fit_results[(zbin, morph, 'single')] = {
+                    #    'sampler': sampler_single,
+                    #    'params_50': np.percentile(samples_single, 50, axis=0),
+                    #    'params_16': np.percentile(samples_single, 16, axis=0),
+                    #    'params_84': np.percentile(samples_single, 84, axis=0)
+                    #}
+                    #print(f'Done single Schechter fit for {morph} at z={zbin}')
 
                     # DPL Fit
-                    initial_guess_DPL = [10.5, -1.5, -2.0, -2.5]
+                    initial_guess_DPL = [10.5, -1.5, -2.0,-3.0]
                     pos_DPL = pos_func_DPL(initial_guess_DPL)
                     sampler_DPL = emcee.EnsembleSampler(nwalkers_DPL, ndim_DPL, log_probability_DPL, args=(logM, Phi, dPhi))
                     sampler_DPL.run_mcmc(pos_DPL, 60000, progress=True)
@@ -169,14 +169,14 @@ def fit_MCMC(smf_morph, path_out, filename, fit_range=(9, 12)):
         }
 
     # Save the results to a pickle file
-    file_path = os.path.join(path_out, filename + '_fit_results_' + str(fit_range[0]) + '.pkl')
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
+    #file_path = os.path.join(path_out, filename + '_fit_results_' + str(fit_range[0]) + '.pkl')
+    #os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
 
     # Serialize the parameters-only dictionary
-    with open(file_path, 'wb') as file:
-        pickle.dump(params_only_results, file)
+    #with open(file_path, 'wb') as file:
+    #    pickle.dump(params_only_results, file)
 
-    print(f"Parameters-only fit results stored successfully at {file_path}")
+    #print(f"Parameters-only fit results stored successfully at {file_path}")
 
     # Save the sampler chains in HDF5 format
     samples_out_dir = os.path.join(path_out, 'samplers')
@@ -189,8 +189,8 @@ def fit_MCMC(smf_morph, path_out, filename, fit_range=(9, 12)):
 
 # Example usage with your defined morphology classes and redshift bins:
 morph_class = ['irr', 'disk', 'sph', 'db','reg','comp','all']
-zbins = [0.2, 0.5, 0.8, 1.1, 1.5, 2, 2.5, 3, 3.5, 4.5, 5.5,6.5]
-
+#zbins = [0.2, 0.5, 0.8, 1.1, 1.5, 2, 2.5, 3, 3.5, 4.5, 5.5,6.5]
+zbins = [3.5,4.5,5.5,6.5]
 path_in = '/n03data/huertas/COSMOS-Web/SMF'
 os.makedirs(path_in, exist_ok=True)
 smf_files = ['smf_morph_3.1_allerrors_nocompact']
