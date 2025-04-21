011ef17168b098724964a1e5c1123633f31f1de5
mhuertascompany <marc.huertas@obspm.fr>
2025-04-21 13:06:58 +0200

    revised

--- adversarial/SMFs/MCMC_SMF_3models.py
+++ adversarial/SMFs/MCMC_SMF_3models.py
@@ -194,7 +194,9 @@ zbins = [0.2, 0.5, 0.8, 1.1, 1.5, 2, 2.5, 3, 3.5, 4.5, 5.5,6.5]
 path_in = '/n03data/huertas/COSMOS-Web/SMF'
 os.makedirs(path_in, exist_ok=True)
 #smf_files = ['smf_morph_3.1_allerrors_nocompactphys_0.07F444','smf_morph_3.1_allerrors_nocompactphys_0.07F444_Q','smf_morph_3.1_allerrors_nocompactphys_0.07F444_SF']
-smf_files = ['smf_morph_3.1_allerrors_nocompactphys_0.07F444_SF']
+#smf_files = ['smf_morph_3.1_allerrors_nocompactphys_0.07F444_SF']
+smf_files = []
+
 
 for smf_type in smf_files:
     smf_file = os.path.join(path_in, smf_type + '.pkl')
