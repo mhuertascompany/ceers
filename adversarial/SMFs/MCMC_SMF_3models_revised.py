b8e077be7f18a29dfe5e632b7f34233aa06d5f66
mhuertascompany <marc.huertas@obspm.fr>
2025-04-24 16:41:35 +0100

    SMF fit all again

--- adversarial/SMFs/MCMC_SMF_3models_revised.py
+++ adversarial/SMFs/MCMC_SMF_3models_revised.py
@@ -179,7 +179,7 @@ def fit_MCMC(smf_morph, path_out, filename, fit_range=(9, 12)):
     #print(f"Parameters-only fit results stored successfully at {file_path}")
 
     # Save the sampler chains in HDF5 format
-    samples_out_dir = os.path.join(path_out, 'samplers_revised')
+    samples_out_dir = os.path.join(path_out, 'samplers_revised_all')
     os.makedirs(samples_out_dir, exist_ok=True)
     for (zbin, morph, model), results in fit_results.items():
         h5_file = os.path.join(samples_out_dir, filename + f'_{zbin}_{morph}_{model}_{fit_range[0]}_sampler.h5')
@@ -195,7 +195,7 @@ path_in = '/n03data/huertas/COSMOS-Web/SMF'
 os.makedirs(path_in, exist_ok=True)
 #smf_files = ['smf_morph_3.1_allerrors_nocompactphys_0.07F444','smf_morph_3.1_allerrors_nocompactphys_0.07F444_Q','smf_morph_3.1_allerrors_nocompactphys_0.07F444_SF']
 #smf_files = ['smf_morph_3.1_allerrors_nocompactphys_0.07F444_SF']
-smf_files = ["smf_morph_3.1_allerrors_nocompactphys_noqsos_nolrds_noXray_Q"]
+smf_files = ["smf_morph_3.1_allerrors_nocompactphys_noqsos_nolrds_noXray"]
 
 
 for smf_type in smf_files:
