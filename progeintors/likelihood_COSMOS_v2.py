bf997383df52ff79213cb038a866901e395687b5
Laura Marrero <alu0101442204@ull.edu.es>
2026-05-19 19:05:26 +0100

    update

--- progeintors/likelihood_COSMOS_v2.py
+++ progeintors/likelihood_COSMOS_v2.py
@@ -500,8 +500,10 @@ loaded_model = DataModule.load_from_checkpoint(checkpoint_path,map_location='cpu
 loaded_model.eval()
 
 
+print("Loading cosmos catalog safely...")
 cosmos_data_path = "/scratch/lmarrero-ext/likelihood_COSMOS_SFR/"
-cosmos_cat = pd.read_csv(cosmos_data_path+"COSMOSWeb_Laura_processed_SFH.csv") # Data from COSMOS-WEB, converted from .fits to .csv in florah_eval_SFR.ipynb
+cosmos_cat = pd.read_csv(cosmos_data_path+"COSMOSWeb_Laura_processed_SFH.csv", engine='python') # Data from COSMOS-WEB, converted from .fits to .csv in florah_eval_SFR.ipynb
+print("Catalog loaded successfully!")
 
 nfm_data_path = "/scratch/lmarrero-ext/likelihood_COSMOS_SFR/node_features_morphology/"
 
